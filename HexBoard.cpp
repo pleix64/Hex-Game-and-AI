4f8577c1cf8d7849d6ceddcdabf52e685f0f2cae
Yu Chen <chenyu.bnu@gmail.com>
2022-01-29 04:14:15 -0700

    Legal moves; judge whether there is a winner, and who is the winner

--- HexBoard.cpp
+++ HexBoard.cpp
@@ -7,17 +7,31 @@
 //
 
 #include <iostream>
+#include <algorithm>
+//#include <functional>
 #include "HexBoard.hpp"
 
 using namespace std;
+//using namespace std::placeholders;
+
+ostream& operator<<(ostream& out, const COLOR& color) {
+    if(color==COLOR::RED)
+        out << "RED";
+    else if(color==COLOR::BLUE)
+        out << "BLUE";
+    else
+        out << "WHIHE";
+    return out;
+}
 
 HexBoard::HexBoard(int side): N(side),V(side*side) {
-    adj = new set<int>[V];
+    col = vector<COLOR>(V,COLOR::WHITE);
+    adj = new map<int,COLOR>[V];
     for(int i=0; i<V; ++i) {
         int x = getX(i);
         int y = getY(i);
-        int nx, ny;
         // six neighbors
+        int nx, ny;
         nx = x-1; ny = y;
         add_neighbor_safe(i, nx, ny);
         nx = x-1; ny = y+1;
@@ -37,12 +51,102 @@ HexBoard::~HexBoard() {
     delete [] adj;
 }
 
+bool HexBoard::adjacent(int i, int j) {
+    auto it = adj[i].find(j);
+    return it!=adj[i].end();
+}
+
+
+void HexBoard::player_move(COLOR player, int x, int y) {
+    if(player==COLOR::WHITE) {
+        cout << "Player's color must be "
+        "either COLOR::RED or COLOR::BLUE." << endl;
+        return;
+    }
+    int i = getNode(x, y);
+    // legal move
+    if(x>=0 && x<N && y>=0 && y<N && col[i]==COLOR::WHITE)
+        col[i] = player;
+    else {
+        cout << "Illegal move. \nPlease select an available location.\n";
+        return;
+    }
+    
+    // if neighbor is already with the same color
+    // change the edge to this neighbor as the player's color
+    // FIXME: it may turn out that no need for edge COLOR
+    for(auto it=adj[i].begin(); it!=adj[i].end(); ++it) {
+        if(col[it->first]==player) {
+            it->second = player;
+            adj[it->first][i] = player;
+        }
+    }
+        
+    // add this hexagon to player's open set
+    int dist;
+    if(player==COLOR::RED)
+        dist = getX(i);
+    else if(player==COLOR::BLUE)
+        dist = getY(i);
+    open[player].insert(make_pair(dist, i));
+}
+
+bool HexBoard::player_won(COLOR& player) {
+    bool won = false;
+    for(auto p:{COLOR::RED, COLOR::BLUE}){
+        //add player owned nodes on North Side (RED) or West Side (BLUE)
+        for(auto it=open[p].begin(); it!=open[p].end(); ) {
+            if(it->first==0) {
+                closed[p].insert(it->second);
+                it = open[p].erase(it);
+            }
+            else
+                break;
+        }
+        // move open set elements which are closed set elements' neighbors
+        // to closed set
+        auto it=open[p].begin();
+        while(it!=open[p].end()) {
+            //find_if(closed[p].begin(),closed[p].end(),
+            //        bind(adjacent(_1, it->second)));
+            auto it_adj =
+            find_if(closed[p].begin(),closed[p].end(),
+                    [&](int x) {return this->adjacent(x, it->second);});
+            if(it_adj!=closed[p].end()) {
+                closed[p].insert(it->second);
+                it = open[p].erase(it);
+            }
+            else
+                ++it;
+        }
+        // test whether one of players won
+        set<int>::iterator it_farside;
+        if(p==COLOR::RED)
+            it_farside = find_if(closed[p].begin(),closed[p].end(),
+                                 [&](int i){return this->getX(i)==N-1;});
+        else if(p==COLOR::BLUE)
+            it_farside = find_if(closed[p].begin(),closed[p].end(),
+                                 [&](int i){return this->getY(i)==N-1;});
+        
+        if(it_farside!=closed[p].end()) {
+            won = true;
+            player = p;
+        }
+    }
+    
+    return won;
+}
+
 void HexBoard::print() {
     cout << "Adjacency List \n";
     for (int i = 0; i < V; ++i) {
         cout << i << ": ";
-        for (auto it = adj[i].begin(); it != adj[i].end(); ++it)
-            cout << *it << ", ";
+        for (auto it = adj[i].begin(); it != adj[i].end(); ++it) {
+            cout << it->first;
+            if(it->second!=COLOR::WHITE)
+                cout << "(" << it->second << ")";
+            cout << ", ";
+        }
         cout << endl;
     }
 }
--- HexBoard.hpp
+++ HexBoard.hpp
@@ -21,37 +21,70 @@ Coordinate convention in this program:
 #ifndef HexBoard_hpp
 #define HexBoard_hpp
 
+#include <iostream>
+#include <map>
 #include <set>
+#include <vector>
 
 using namespace std;
+
+enum class COLOR: short {WHITE, RED, BLUE};
+
+ostream& operator<<(ostream& out, const COLOR& color);
 
 class HexBoard {
 public:
     HexBoard(int side);
+    
     ~HexBoard();
+    
     int getX(int i) {
         return i / N;
     }
+    
     int getY(int i) {
         return i % N;
     }
+    
     int getNode(int x, int y) {
         return x*N + y;
     }
+    
     void add_neighbor_safe(int i, int x, int y) {
         // add node (x,y) to i's neighbor safely
         // make sure the new neighbor is not outside the board
         // if it's out of the range, nothing is added.
         if(x>=0 && x<N && y>=0 && y<N)
-             adj[i].insert(getNode(x,y));
+             adj[i].insert(make_pair(getNode(x,y),COLOR::WHITE));
     }
+    
+    bool adjacent(int i, int j);
+    // test whether there is an edge from node i to node j
+    
+    void player_move(COLOR player, int x, int y);
+    // assign the hexagon (x,y) with COLOR player (RED or BLUE)
+    // ensure it is a legal move (not already occupied and in board range)
+    // change edge color accordingly if neighbor nodes have the same color
+    
+    bool player_won(COLOR& player);
+    // return true is one of the players won the game,
+    // and assign winner's COLOR to player
     
     void print();
     
 private:
     int N; // side length
     int V; // number of nodes
-    set<int> * adj; // adjancency list, no weight needed
+    vector<COLOR> col; // node values of COLOR
+    map<int,COLOR> * adj; // adjancency list, edge value is COLOR
+    map<COLOR,set<pair<int,int> > > open; // each player's open set
+    // each element in open set is a pair of node index (second)
+    // and its distance to North or West Side (first)
+    // once a hexagon is occupied, first add it to player's open set
+    map<COLOR,set<int>> closed; // each player's closed set
+    // 1. add player owned nodes on North Side (RED) or West Side (BLUE),
+    // 2. move open set elements which are closed set elements' neighbors
+    //    to closed set
 };
 
 #endif /* HexBoard_hpp */
